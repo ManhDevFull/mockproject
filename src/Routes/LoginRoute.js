147644939a26ff400dc2c3224972d06bdb664599
ManhDevFull <163883053+ManhDevFull@users.noreply.github.com>
2024-08-15 16:20:21 +0700

    Update LoginRoute.js

--- src/Routes/LoginRoute.js
+++ src/Routes/LoginRoute.js
@@ -1,8 +1,14 @@
 import LoginPage from "../Pages/ClientPages/LoginPage/LoginPage.js"
+import LoginPage from "../Pages/ClientPages/HomePage/HomePage.js"
 const LoginRoute=[
     {
         path:'/signin/client',
         element: LoginPage
-    }
+    },
+    {
+        path:'/',
+        element: HomePage
+    },
+    
 ]
 export default LoginRoute

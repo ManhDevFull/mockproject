25329d9f18b4410a91396a8a48362f6c6904fb66
ManhDevFull <163883053+ManhDevFull@users.noreply.github.com>
2024-08-15 16:22:59 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './App.css'
 import AdminLayout from './Layout/AdminLayout/Index'
 import LayoutDefault from './Layout/DefaultLayout/Index'
 import { Routes, Route } from "react-router-dom"
-import { LoginRoute } from './Routes/index'
+import { UserRoute } from './Routes/index'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<LayoutDefault />} >
-          {LoginRoute.map((route, index) => { return <Route key={index} path={route.path} element={<route.element />} /> })}
+          {UserRoute.map((route, index) => { return <Route key={index} path={route.path} element={<route.element />} /> })}
         </Route>
         <Route path="/admin" element={<AdminLayout />} >
 
